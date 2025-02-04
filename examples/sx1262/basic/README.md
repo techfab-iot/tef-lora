4652f0e62cb2d8327fd633431bcfaac107854aec
Magnatto <renato.fdmferreira@gmail.com>
2025-02-04 16:58:59 -0300

    feat: adiciona 1262 do nopnop

---
+++ examples/sx1262/basic/README.md
@@ -0,0 +1,7 @@
+# Basic Example   
+Simple send or receive.   
+
+# Configuration   
+![config-app](https://user-images.githubusercontent.com/6020549/162125873-556d92c4-60aa-46fb-9f6e-d2ff38858f58.jpg)
+![config-basic-1](https://github.com/user-attachments/assets/2b40f2e0-d951-4659-b6aa-4b3618f74df5)
+
---
+++ examples/sx1262/basic/main/app/main.c
@@ -0,0 +1,125 @@
+/* The example of ESP-IDF
+ *
+ * This sample code is in the public domain.
+ */
+
+#include <inttypes.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "sx1262.h"
+
+static const char *TAG = "MAIN";
+
+#if CONFIG_SENDER
+void task_tx(void *pvParameters) {
+  ESP_LOGI(pcTaskGetName(NULL), "Start");
+  uint8_t buf[256];  // Maximum Payload size of SX1261/62/68 is 255
+  while (1) {
+    TickType_t nowTick = xTaskGetTickCount();
+    int txLen = sprintf((char *)buf, "Hello World %" PRIu32, nowTick);
+    ESP_LOGI(pcTaskGetName(NULL), "%d byte packet sent...", txLen);
+
+    // Wait for transmission to complete
+    if (LoRaSend(buf, txLen, SX126x_TXMODE_SYNC) == false) {
+      ESP_LOGE(pcTaskGetName(NULL), "LoRaSend fail");
+    }
+
+    // Do not wait for the transmission to be completed
+    // LoRaSend(buf, txLen, SX126x_TXMODE_ASYNC );
+
+    int lost = GetPacketLost();
+    if (lost != 0) {
+      ESP_LOGW(pcTaskGetName(NULL), "%d packets lost", lost);
+    }
+
+    vTaskDelay(pdMS_TO_TICKS(1000));
+  }  // end while
+}
+#endif  // CONFIG_SENDER
+
+#if CONFIG_RECEIVER
+void task_rx(void *pvParameters) {
+  ESP_LOGI(pcTaskGetName(NULL), "Start");
+  uint8_t buf[256];  // Maximum Payload size of SX1261/62/68 is 255
+  while (1) {
+    uint8_t rxLen = LoRaReceive(buf, sizeof(buf));
+    if (rxLen > 0) {
+      ESP_LOGI(
+        pcTaskGetName(NULL), "%d byte packet received:[%.*s]", rxLen, rxLen,
+        buf);
+
+      int8_t rssi, snr;
+      GetPacketStatus(&rssi, &snr);
+      ESP_LOGI(pcTaskGetName(NULL), "rssi=%d[dBm] snr=%d[dB]", rssi, snr);
+    }
+    vTaskDelay(1);  // Avoid WatchDog alerts
+  }  // end while
+}
+#endif  // CONFIG_RECEIVER
+
+void app_main() {
+  // Initialize LoRa
+  LoRaInit();
+  int8_t txPowerInDbm = 22;
+
+  uint32_t frequencyInHz = 0;
+#if CONFIG_433MHZ
+  frequencyInHz = 433000000;
+  ESP_LOGI(TAG, "Frequency is 433MHz");
+#elif CONFIG_866MHZ
+  frequencyInHz = 866000000;
+  ESP_LOGI(TAG, "Frequency is 866MHz");
+#elif CONFIG_915MHZ
+  frequencyInHz = 915000000;
+  ESP_LOGI(TAG, "Frequency is 915MHz");
+#elif CONFIG_OTHER
+  ESP_LOGI(TAG, "Frequency is %dMHz", CONFIG_OTHER_FREQUENCY);
+  frequencyInHz = CONFIG_OTHER_FREQUENCY * 1000000;
+#endif
+
+#if CONFIG_USE_TCXO
+  ESP_LOGW(TAG, "Enable TCXO");
+  float tcxoVoltage = 3.3;      // use TCXO
+  bool useRegulatorLDO = true;  // use DCDC + LDO
+#else
+  ESP_LOGW(TAG, "Disable TCXO");
+  float tcxoVoltage = 0.0;       // don't use TCXO
+  bool useRegulatorLDO = false;  // use only LDO in all modes
+#endif
+
+  // LoRaDebugPrint(true);
+  if (
+    LoRaBegin(frequencyInHz, txPowerInDbm, tcxoVoltage, useRegulatorLDO) != 0) {
+    ESP_LOGE(TAG, "Does not recognize the module");
+    while (1) {
+      vTaskDelay(1);
+    }
+  }
+
+  uint8_t spreadingFactor = 7;
+  uint8_t bandwidth = 4;
+  uint8_t codingRate = 1;
+  uint16_t preambleLength = 8;
+  uint8_t payloadLen = 0;
+  bool crcOn = true;
+  bool invertIrq = false;
+#if CONFIG_ADVANCED
+  spreadingFactor = CONFIG_SF_RATE;
+  bandwidth = CONFIG_BANDWIDTH;
+  codingRate = CONFIG_CODING_RATE;
+#endif
+  LoRaConfig(
+    spreadingFactor, bandwidth, codingRate, preambleLength, payloadLen, crcOn,
+    invertIrq);
+
+#if CONFIG_SENDER
+  xTaskCreate(&task_tx, "TX", 1024 * 4, NULL, 5, NULL);
+#endif
+#if CONFIG_RECEIVER
+  xTaskCreate(&task_rx, "RX", 1024 * 4, NULL, 5, NULL);
+#endif
+}
